fd1819afa1c2eee25897f0559f92d32147f97f37
Kenneth S <kksiu@wisc.edu>
2015-01-12 11:26:40 -0600

    Finished a simple shader manager

--- Handlers/ShaderManager.cpp
+++ Handlers/ShaderManager.cpp
@@ -1,5 +1,10 @@
 #include "ShaderManager.h"
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 ShaderManager::ShaderManager()
 {
@@ -8,4 +13,108 @@ ShaderManager::ShaderManager()
 
 ShaderManager::~ShaderManager()
 {
+}
+
+void ShaderManager::loadShader(std::string fileName, std::string keyName)
+{
+    std::shared_ptr<sf::Shader> shader = std::make_shared<sf::Shader>();
+    
+    //shader loading passed
+    if(shader->loadFromFile("./Shaders/" + fileName + ".vert",
+                         "./Shaders/" + fileName + ".frag"))
+    {
+        //add to list
+        mSFMLShaders[keyName] = shader;
+    }
+}
+
+void ShaderManager::loadGLShader(std::string fileName, std::string keyName)
+{
+    mGLShaders[keyName] = loadGLShaderHelper("./Shaders/" + fileName + ".vert", "./Shaders/" + fileName + ".vert");
+}
+
+GLuint ShaderManager::getGLShader(std::string keyName)
+{
+    return mGLShaders[keyName];
+}
+
+std::shared_ptr<sf::Shader> ShaderManager::getShader(std::string keyName)
+{
+    return mSFMLShaders[keyName];
+}
+
+GLuint ShaderManager::loadGLShaderHelper(std::string vert, std::string frag)
+{
+    GLuint vertShader = glCreateShader(GL_VERTEX_SHADER);
+    GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+    
+    // Read shaders
+    std::string vertShaderStr = readFile(vert.c_str());
+    std::string fragShaderStr = readFile(frag.c_str());
+    const char *vertShaderSrc = vertShaderStr.c_str();
+    const char *fragShaderSrc = fragShaderStr.c_str();
+    
+    GLint result = GL_FALSE;
+    int logLength;
+    
+    // Compile vertex shader
+    std::cout << "Compiling vertex shader." << std::endl;
+    glShaderSource(vertShader, 1, &vertShaderSrc, NULL);
+    glCompileShader(vertShader);
+    
+    // Check vertex shader
+    glGetShaderiv(vertShader, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(vertShader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<GLchar> vertShaderError((logLength > 1) ? logLength : 1);
+    glGetShaderInfoLog(vertShader, logLength, NULL, &vertShaderError[0]);
+    std::cout << &vertShaderError[0] << std::endl;
+    
+    // Compile fragment shader
+    std::cout << "Compiling fragment shader." << std::endl;
+    glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
+    glCompileShader(fragShader);
+    
+    // Check fragment shader
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &result);
+    glGetShaderiv(fragShader, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<GLchar> fragShaderError((logLength > 1) ? logLength : 1);
+    glGetShaderInfoLog(fragShader, logLength, NULL, &fragShaderError[0]);
+    std::cout << &fragShaderError[0] << std::endl;
+    
+    std::cout << "Linking program" << std::endl;
+    GLuint program = glCreateProgram();
+    glAttachShader(program, vertShader);
+    glAttachShader(program, fragShader);
+    glLinkProgram(program);
+    
+    glGetProgramiv(program, GL_LINK_STATUS, &result);
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+    std::vector<char> programError( (logLength > 1) ? logLength : 1 );
+    glGetProgramInfoLog(program, logLength, NULL, &programError[0]);
+    std::cout << &programError[0] << std::endl;
+    
+    glDeleteShader(vertShader);
+    glDeleteShader(fragShader);
+    
+    return program;
+}
+
+std::string ShaderManager::readFile(const char* filePath)
+{
+    std::string content;
+    std::ifstream fileStream(filePath, std::ios::in);
+    
+    if(!fileStream.is_open()) {
+        std::cerr << "Could not read file " << filePath << ". File does not exist." << std::endl;
+        return "";
+    }
+    
+    std::string line = "";
+    while(!fileStream.eof()) {
+        std::getline(fileStream, line);
+        content.append(line + "\n");
+    }
+    
+    fileStream.close();
+    return content;
 }
--- Handlers/ShaderManager.h
+++ Handlers/ShaderManager.h
@@ -1,8 +1,35 @@
 #pragma once
+
+#include <string>
+#include <map>
+#include <memory>
+
+#include <GL/glew.h>
+#include <SFML/Graphics.hpp>
+
 class ShaderManager
 {
 public:
-	ShaderManager();
-	~ShaderManager();
+    //load and retrieve the shaders
+    void loadShader(std::string fileName, std::string keyName);
+    void loadGLShader(std::string fileName, std::string keyName);
+    
+    GLuint getGLShader(std::string keyName);
+    std::shared_ptr<sf::Shader> getShader(std::string keyName);
+    
+    static ShaderManager& getInstance()
+    {
+        static ShaderManager instance;
+        return instance;
+    }
+    
+private:
+    ShaderManager();
+    ~ShaderManager();
+    std::map<std::string, GLuint> mGLShaders;
+    std::map<std::string, std::shared_ptr<sf::Shader>> mSFMLShaders;
+    
+    //used http://www.nexcius.net/2012/11/20/how-to-load-a-glsl-shader-in-opengl-using-c/ for a function to load a shader
+    GLuint loadGLShaderHelper(std::string vert, std::string);
+    std::string readFile(const char* filePath);
 };
-
